a98996159b24810ef5135df0add82e6b5f09f39f
hangvu <thuhangvu0302@gmail.com>
2024-04-08 11:40:33 +1000

    Initial Commit

---
+++ BubblePopGame/BubblePopGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblePopGameApp.swift
+//  BubblePopGame
+//
+//  Created by Hang Vu on 8/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct BubblePopGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
